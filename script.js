e2fb085765cc6d83c530a680c8d0af6807fc92bb
Julien Baouz <baouzjulien@gmail.com>
2025-11-26 14:48:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const poll = new Map();
 
+function addOption(option) {
+  if(!poll.has(option)) {
+    const initSet = new Set();
+    poll.add(option, initSet)
+    return `Option "${option}" added to the poll.`
+  }
+  return `Option "${option}" already exists.`
+}
